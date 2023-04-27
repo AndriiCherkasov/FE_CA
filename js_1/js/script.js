fe94996c08ee03d47afa963b208b2b783b7ca0bd
root <root@DESKTOP-UM3QAGD.localdomain>
2023-04-27 16:32:11 +0300

    add hw_js_1

---
+++ js_1/js/script.js
@@ -0,0 +1,42 @@
+//minimum
+
+let Name = 'Andrii';
+let Surname = 'Cherkasov';
+let fullName = Name + ' ' + Surname;
+console.log(fullName);
+
+// wrong names
+// let qwerty;
+// let 12345;
+// let firstName;
+// let secondName;
+
+// Коментар в один рядок
+
+/*
+* Коментар
+* в декілька  
+* рядків
+*/
+
+let a = 2;
+let b = 3;
+
+let c, d;
+c = 2;
+d = 3;
+
+const City = 'Kyiv';
+
+//norm
+
+let firstName = prompt('Your Name');
+console.log(firstName);
+
+let birthDate = prompt('Your birth year');
+let currentYear = '2023'
+console.log(currentYear - birthDate);
+
+let sideSquare = prompt('Length of the side of the square');
+let squarePerimeter = sideSquare * 4;
+console.log(squarePerimeter);
